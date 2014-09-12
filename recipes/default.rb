283cd8890dbbed287e6a895710cbc70627bb5118
James Cuzella <james.cuzella@lyraphase.com>
2014-09-12 16:53:44 -0600

    Add initial fisa-vim-config repo checkout to recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,20 @@
 # Copyright (C) 2014 James Cuzella [@trinitronx](https://github.com/trinitronx/)
 #
 # 
+unless system('vim --version | grep +python')
+  raise 'You need a version of vim compiled with python support!'
+end
+
+git "#{Chef::Config[:file_cache_path]}/app_name" do
+  repository node['fisa-vim-config']['repo_url']
+  revision node['fisa-vim-config']['git_revision']
+  action :sync
+  notifies :run, "link[install_vimrc]"
+end
+
+# TODO: Install vimrc
+#link "" do
+#  to ""
+#end
+
+## TODO: DO something similar to install or upgrade the fisa-vim-config install
